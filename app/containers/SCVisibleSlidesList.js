b7b35f4aaa6025dd27f6dc6adba76460c195160a
Riccardo Massari <maxdrift85@gmail.com>
2018-10-07 18:14:34 +0200

    Use 'reselect' to improve selector's performance

--- app/containers/SCVisibleSlidesList.js
+++ app/containers/SCVisibleSlidesList.js
@@ -2,32 +2,15 @@ import { connect } from 'react-redux';
 import {
   updateSlideIndex,
   toggleSlideSelected,
-  toggleSlideState,
-  ExcludedState,
-  FixedState,
-  JuryState
+  toggleSlideState
 } from '../actions/slides';
 import { VisibilityFilters } from '../actions/visibilityFilter';
 import { openSubjectsModal } from '../actions/subjectsModal';
+import { getVisibleSlides } from '../selectors';
 import SCSortableSlidesList from '../components/SCSortableSlidesList';
 
-const getVisibleSlides = (slides, filter) => {
-  switch (filter) {
-    case VisibilityFilters.SHOW_ALL:
-      return slides;
-    case VisibilityFilters.SHOW_EXCLUDED:
-      return slides.filter(slide => slide.state === ExcludedState);
-    case VisibilityFilters.SHOW_FIXED:
-      return slides.filter(slide => slide.state === FixedState);
-    case VisibilityFilters.SHOW_JURY:
-      return slides.filter(slide => slide.state === JuryState);
-    default:
-      return slides;
-  }
-};
-
 const mapStateToProps = state => ({
-  slides: getVisibleSlides(state.slides, state.visibilityFilter),
+  slides: getVisibleSlides(state),
   // Disable drag&drop reordering if not in "View all" slides view.
   reorderingEnabled: state.visibilityFilter === VisibilityFilters.SHOW_ALL
 });
---
+++ app/selectors/index.js
@@ -0,0 +1,31 @@
+import { createSelector } from 'reselect';
+import { ExcludedState, FixedState, JuryState } from '../actions/slides';
+import { VisibilityFilters } from '../actions/visibilityFilter';
+
+const getVisibilityFilter = state => state.visibilityFilter;
+const getSlides = state => state.slides;
+
+export const getVisibleSlides = createSelector(
+  [getVisibilityFilter, getSlides],
+  (visibilityFilter, slides) => {
+    switch (visibilityFilter) {
+      case VisibilityFilters.SHOW_ALL:
+        return slides;
+      case VisibilityFilters.SHOW_EXCLUDED:
+        return slides.filter(slide => slide.state === ExcludedState);
+      case VisibilityFilters.SHOW_FIXED:
+        return slides.filter(slide => slide.state === FixedState);
+      case VisibilityFilters.SHOW_JURY:
+        return slides.filter(slide => slide.state === JuryState);
+      default:
+        return slides;
+    }
+  }
+);
+
+export const selectedSlideIds = createSelector([getSlides], slides =>
+  slides.reduce(
+    (selected, slide) => (slide.selected ? [...selected, slide.id] : selected),
+    []
+  )
+);
