4a951d9574daf5c0c517cf258b0f2fe9de7f237a
Riccardo Massari <maxdrift85@gmail.com>
2018-10-10 00:43:58 +0200

    Implement check for orphan images at startup

--- app/containers/App.js
+++ app/containers/App.js
@@ -1,13 +1,16 @@
 // @flow
-import * as React from 'react';
+import { remote } from 'electron';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import fs from 'fs';
+import { connect } from 'react-redux';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
 import blue from '@material-ui/core/colors/blue';
 import red from '@material-ui/core/colors/red';
+import { addSlidesAsync } from '../actions/slides';
 
-type Props = {
-  children: React.Node
-};
+const ga = remote.getGlobal('ga');
 
 const theme = createMuiTheme({
   palette: {
@@ -16,8 +19,12 @@ const theme = createMuiTheme({
   }
 });
 
-export default class App extends React.Component<Props> {
+class App extends Component {
   props: Props;
+
+  componentDidMount() {
+    this.props.reloadSlides(this.props.slidesNeedingReload);
+  }
 
   render() {
     return (
@@ -31,3 +38,32 @@ export default class App extends React.Component<Props> {
     );
   }
 }
+
+App.propTypes = {
+  children: PropTypes.node.isRequired,
+  reloadSlides: PropTypes.func.isRequired,
+  slidesNeedingReload: PropTypes.arrayOf(PropTypes.string).isRequired
+};
+
+const hasMissingSrcFile = srcSet =>
+  srcSet.some(src => !fs.existsSync(src.split(/( \d+w$)/)[0]));
+
+const slidesNeedingReload = slides =>
+  slides.reduce((acc, slide) => {
+    if (fs.existsSync(slide.src) && hasMissingSrcFile(slide.srcSet)) {
+      ga.event('Slides', 'missing thumbnail').send();
+      console.log('Found broken slide:', slide.src);
+      return [...acc, slide.src];
+    }
+    return acc;
+  }, []);
+
+const mapStateToProps = state => ({
+  slidesNeedingReload: slidesNeedingReload(state.slides)
+});
+
+const mapDispatchToProps = dispatch => ({
+  reloadSlides: paths => dispatch(addSlidesAsync(paths))
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
